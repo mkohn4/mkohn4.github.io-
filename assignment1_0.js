1a298fb500e9535fba94d75a0236f2fd45690c28
mkohn4 <35665118+mkohn4@users.noreply.github.com>
2018-02-04 14:34:16 -0800

    Update assignment1_0.js

--- assignment1_0.js
+++ assignment1_0.js
@@ -1,1 +1,4 @@
-document.write("<span style = 'color: red; font-size: 22px; font-family: Times New Roman;'><strong>ab </strong></span>" + "<span style = 'color: blue; font-size: 22px; font-family: Arial;'>12 </span>" + "<span style = 'color: green; font-size: 22px; font-family: Impact;'><em>34</em></span>")
+var process;
+function process(){
+  document.write("<span style = 'color: red; font-size: 22px; font-family: Times New Roman;'><strong>ab </strong></span>" + "<span style = 'color: blue; font-size: 22px; font-family: Arial;'>12 </span>" + "<span style = 'color: green; font-size: 22px; font-family: Impact;'><em>34</em></span>");
+}
