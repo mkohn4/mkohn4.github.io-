5fd95964d9dd715f1694114f1ece3fc06e2d003a
mkohn4 <35665118+mkohn4@users.noreply.github.com>
2018-02-04 14:25:13 -0800

    Add files via upload

---
+++ assignment1_0.js
@@ -0,0 +1,1 @@
+document.write("<span style = 'color: red; font-size: 22px; font-family: Times New Roman;'><strong>ab </strong></span>" + "<span style = 'color: blue; font-size: 22px; font-family: Arial;'>12 </span>" + "<span style = 'color: green; font-size: 22px; font-family: Impact;'><em>34</em></span>")
